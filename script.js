e8a5826b62aaf96503bc3cc1184a9923748b3d2e
jax wong <113402506+airair21@users.noreply.github.com>
2023-03-27 00:09:24 -0400

    bro

--- script.js
+++ script.js
@@ -1068,6 +1068,7 @@ const hairs = [
     const graveHTML = `
     
     <a href="airair21.github.io/tem/obituary.html#${hair.element}">
+    
       <div class="headstone">
         <img src="${hair.image_url}">
       </div>
